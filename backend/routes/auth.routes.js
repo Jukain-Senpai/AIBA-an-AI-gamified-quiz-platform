47f3db68ba28243e058418674a17a197e12f0762
Jukain-Senpai <vundhgcd230066@fpt.edu.vn>
2026-01-12 06:30:28 +0700

    Add JWT authentication middleware and protected routes

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -5,8 +5,16 @@ const { testAuth,
      register,
      login, } = require('../controllers/auth.controller');
 
+     const protect = require("../middleware/auth.middleware");
 router.get("/test", testAuth);
 router.post("/register", register);
 router.post("/login", login);
+
+router.get("/profile", protect, (req, res) => {
+    res.json({
+        message: "Access granted",
+        user: req.user,
+    });
+});
 
 module.exports = router;
